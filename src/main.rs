1d105fe93f206ba9a4a2df8a074c96c375bca18b
crazyoptimist <hey@crazyoptimist.net>
2024-03-20 13:07:30 -0500

    feat: response without header, body

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    // println!("Request:\n{}", String::from_utf8_lossy(&buffer[..]))
+
+    // Here's how to compose a HTTP response
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    //
+    // ex: HTTP/1.1 200 OK\r\n\r\n
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
